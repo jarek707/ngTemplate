9aaf5a27098488ea29f6a1857f0f4e42bfc395ea
Jarek Kulikowski <jarek707@gmail.com>
2013-06-23 16:21:40 -0700

    cleanup

--- data/managers.php
+++ data/managers.php
@@ -1,6 +1,6 @@
 <?php
     echo '{ 
             "999" : ["Joseph", "Bloe", "jblo@here.com","555-555-5555"],
-            "8888" :["Moshe", "Cohen", "mcohen@there.com","555-555-5555"]
+            "8888" :["Max", "Białystok", "mcohen@there.com","555-555-5555"]
           }';
 ?>
